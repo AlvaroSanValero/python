11f98ca8b3cfacba2441e59ef6139887f66ec172
Gorka Puente Díez <gorkapuente@usal.es>
2024-04-15 12:56:47 +0200

    movido script a src/

--- src/simuladorRobot.py
+++ src/simuladorRobot.py
@@ -2,13 +2,12 @@ import tkinter as tk
 import math
 
 class RobotSimulator(tk.Tk):
-     def __init__(self):
+    def __init__(self):
         super().__init__()
         self.title("Simulador de Robot")  # Establece el título de la ventana
         self.canvas = tk.Canvas(self, width=600, height=400, bg="white")  # Crea un lienzo (canvas) en la ventana
         self.canvas.pack()  # Empaqueta el lienzo en la ventana
-        self.robot = self.canvas.create_rectangle(295, 195, 305, 205, fill="red")  # Crea un rectángulo rojo para representar al robot
-
+        self.robot = self.canvas.create_rectangle(295, 195, 305, 205, fill="red")  # Crea un rectángulo rojo para representar al robo
         # Variables para los parámetros del robot
         self.ancho_robot = tk.DoubleVar(value=10)
         self.largo_robot = tk.DoubleVar(value=10)
@@ -18,34 +17,34 @@ class RobotSimulator(tk.Tk):
         self.distancia_entre_ruedas = tk.DoubleVar(value=50)
         self.diametro_ruedas = tk.DoubleVar(value=10)
         self.resolucion_encoder = tk.DoubleVar(value=360)
-        
+
         # Widgets Entry para la entrada de parámetros
         tk.Label(self, text="Ancho del Robot:").pack()  # Crea una etiqueta para el ancho del robot
         self.entry_ancho_robot = tk.Entry(self, textvariable=self.ancho_robot, width=10)  # Crea un campo de entrada para el ancho del robot
         self.entry_ancho_robot.pack()  # Empaqueta el campo de entrada en la ventana
-        
+
         tk.Label(self, text="Largo del Robot:").pack()
         self.entry_largo_robot = tk.Entry(self, textvariable=self.largo_robot, width=10)
         self.entry_largo_robot.pack()
-        
+
         tk.Label(self, text="Distancia entre Ruedas:").pack()
         self.entry_distancia_entre_ruedas = tk.Entry(self, textvariable=self.distancia_entre_ruedas, width=10)
         self.entry_distancia_entre_ruedas.pack()
-        
+
         tk.Label(self, text="Diámetro de las Ruedas:").pack()
         self.entry_diametro_ruedas = tk.Entry(self, textvariable=self.diametro_ruedas, width=10)
         self.entry_diametro_ruedas.pack()
-        
+
         tk.Label(self, text="Resolución del Encoder:").pack()
         self.entry_resolucion_encoder = tk.Entry(self, textvariable=self.resolucion_encoder, width=10)
         self.entry_resolucion_encoder.pack()
-        
+
         # Botón para actualizar parámetros
         tk.Button(self, text="Actualizar Parámetros", command=self.actualizar_parametros).pack()
-        
+
          # Asociar evento de teclado a método mover_robot
         self.bind("<KeyPress>", self.mover_robot)
-        
+
     def mover_robot(self, event):
         tecla = event.keysym
         if tecla == 'i':
